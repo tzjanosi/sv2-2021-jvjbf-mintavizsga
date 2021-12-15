fa3f81d8b3ef3483014b2535dacbc50acf188cfa
tibo <tzjanosi@gamma.ttk.pte.hu>
2021-12-15 23:16:59 +0100

    "UniqueFinder" Done

---
+++ sv2-2021-jvjbf-mintavizsga/src/main/java/uniquefinder/UniqueFinder.java
@@ -0,0 +1,18 @@
+package uniquefinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueFinder {
+    public List<Character> getUniqueChars(String input){
+        if(input == null){
+            throw new IllegalArgumentException("Text is null.");
+        }
+        List<Character> temp=new ArrayList<>();
+        for (char c: input.toCharArray()) {
+            if(!temp.contains(c))
+            temp.add(c);
+        }
+        return temp;
+    }
+}
